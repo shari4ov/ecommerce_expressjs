d059d17789697916b870961f584b71c71a6ebc1c
Kenan <kenansharifov400@gmail.com>
2022-08-06 21:21:25 +0400

    sec

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 var cors = require('cors')
 const port = 3000;
 const productAPI = require('./controller/ProductController')
-
+const altCategoryAPI = require("./controller/AltCategoryController");
 app.use(cors())
 app.get('/api/stroyka/get/products/all',productAPI.getProduct)
 app.get('/api/stroyka/get/products/byId/:id',productAPI.getProductByID)
@@ -11,6 +11,8 @@ app.get('/api/stroyka/get/products/byCategory/:category',productAPI.getProductBy
 app.get('/api/stroyka/get/products/bySubCategory/:subCategory',productAPI.getProductBySubCategory)
 app.get('/api/stroyka/get/products/byAltCategory/:altCategory',productAPI.getProductByAltCategory)
 
+app.get('/stroyka/get/altcategories',altCategoryAPI.getAltCategories)
+app.get('/stroyka/get/altcategoriesBySubCatID/:id',altCategoryAPI.getAltCategoriesBySubCategory)
 app.listen(port,() => {
        console.log('Listening');
 })
