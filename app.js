df35f7541ffaf7cd0fab7503e10a0f2c05f4bf25
Kenan <kenansharifov400@gmail.com>
2022-08-06 21:21:02 +0400

    test

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(cors())
 app.get('/api/stroyka/get/products/all',productAPI.getProduct)
 app.get('/api/stroyka/get/products/byId/:id',productAPI.getProductByID)
 app.get('/api/stroyka/get/products/byCategory/:category',productAPI.getProductByCategory)
+app.get('/api/stroyka/get/products/bySubCategory/:subCategory',productAPI.getProductBySubCategory)
+app.get('/api/stroyka/get/products/byAltCategory/:altCategory',productAPI.getProductByAltCategory)
 
 app.listen(port,() => {
        console.log('Listening');
