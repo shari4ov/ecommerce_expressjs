1de4ad6b0a76e9af1bd90a62351d081ac3195883
Kenan <kenansharifov400@gmail.com>
2022-08-07 23:47:44 +0400

    contact us

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const app = express()
 var cors = require('cors')
+var bodyParser = require('body-parser')
 const port = 3000;
 const productAPI = require('./controller/ProductController')
 const altCategoryAPI = require("./controller/AltCategoryController");
 const subCatAPI = require('./controller/SubCategoryController')
 const categoryAPI = require('./controller/CategoryController')
-app.use(cors())
+const contactAPI = require('./controller/ContactController')
+var {body} = require('express-validator');
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 app.get('/api/stroyka/get/products/all',productAPI.getProduct)
 app.get('/api/stroyka/get/products/byId/:id',productAPI.getProductByID)
 app.get('/api/stroyka/get/products/byCategory/:category',productAPI.getProductByCategory)
 app.get('/api/stroyka/get/products/bySubCategory/:subCategory',productAPI.getProductBySubCategory)
 app.get('/api/stroyka/get/products/byAltCategory/:altCategory',productAPI.getProductByAltCategory)
 
-app.get('/stroyka/get/altcategories',altCategoryAPI.getAltCategories)
-app.get('/stroyka/get/altcategoriesBySubCatID/:id',altCategoryAPI.getAltCategoriesBySubCategory)
+app.get('/api/stroyka/get/altcategories',altCategoryAPI.getAltCategories)
+app.get('/api/stroyka/get/altcategoriesBySubCatID/:id',altCategoryAPI.getAltCategoriesBySubCategory)
 
-app.get('/stroyka/get/subcategories',subCatAPI.getSubCats)
-app.get('/stroyka/get/subcategoriesByCatID/:id',subCatAPI.getSubCatsByCatID)
+app.get('/api/stroyka/get/subcategories',subCatAPI.getSubCats)
+app.get('/api/stroyka/get/subcategoriesByCatID/:id',subCatAPI.getSubCatsByCatID)
 
-app.get('/stroyka/get/categories',categoryAPI.getCategories)
+app.get('/api/stroyka/get/categories',categoryAPI.getCategories)
 
-app.listen(port,() => {
+const validationRule = () => {
+       return [
+              body('name').notEmpty().matches(/^[a-zA-Z\s]*$/).escape().trim().withMessage("Ad düzgün doldurulmayıb"),
+              body('email').notEmpty().isEmail().normalizeEmail().escape().trim().withMessage("E-mail  düzgün doldurulmayıb"),
+              body('phone').notEmpty().matches(/^[+]?[0-9]{3}?[(]?[0-9]{2}[)]?[0-9]{4,7}$/).escape().trim().withMessage("Nömrə düzgün doldurulmayıb"),
+              body('message').notEmpty().escape().trim().withMessage("Məktub düzgün deyil")
+       ]
+}
+app.post('/api/stroyka/contactus/',validationRule(),contactAPI.postContact)
+module.exports=app.listen(port,() => {
        console.log('Listening');
 })
---
+++ controller/ContactController.js
@@ -0,0 +1,27 @@
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+const {validationResult} =require('express-validator')
+const { v4: uuidv4 } = require('uuid');
+const postContact = async (req,res,next) => {
+       console.log(req.body);
+       const errors  = validationResult(req)
+       if(!errors.isEmpty()){
+              return res.status(400).json({errors:errors.array()})
+       }
+       await prisma.$connect;
+       let uniq_id__tmp = uuidv4();
+       const contactForm = await prisma.contact.create({
+              data: {
+                     uniq_id:uniq_id__tmp,
+                     email: req.body.email,
+                     name: req.body.name,
+                     phone:req.body.phone,
+                     message: req.body.message
+              }
+       })
+       res.status(200).json({msg: "Successfully submited"});
+}
+
+module.exports = {
+       postContact
+}
